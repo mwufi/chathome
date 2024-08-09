6ce2a8259e4d40c914afe322c35b8ddc8bbb82fa
CZ Tang <mwufi@users.noreply.github.com>
2024-08-09 02:57:24 +0000

    k

--- app/(editing experience)/chat/[roomId]/page.tsx
+++ app/(editing experience)/chat/[roomId]/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import type { User } from "@/party/utils/auth";
 import Link from "next/link";
 import { Room } from "./Room";
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from 'next-auth'
+import { authOptions } from "@/lib/authOptions";
 import type { User } from "@/party/utils/auth";
 import Link from "next/link";
 import Signout from "./Signout";

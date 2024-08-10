2a8c17d79c82b095f334be5dbe1d0b9c1476aee9
Zen <ztang230@gmail.com>
2024-08-10 18:14:09 -0500

    works fine

--- party/editor.ts
+++ party/editor.ts
@@ -1,53 +1,42 @@
 import type * as Party from "partykit/server";
 import { onConnect, type YPartyKitOptions } from "y-partykit";
-import type { Doc } from "yjs";
-import { json, notFound, ok } from "./utils/response";
-import { syncTitle } from "./utils/message";
+import { notFound, ok, json } from "./utils/response";
 
 export default class EditorServer implements Party.Server {
   yjsOptions: YPartyKitOptions = {};
   title: string | undefined;
 
-  constructor(public party: Party.Party) {
-  }
+  constructor(public party: Party.Party) { }
 
-  /** Retrieve messages from room storage and store them on room instance */
-  async loadTitle() {
-    if (!this.title) {
-      this.title =
-        (await this.party.storage.get<string>("title")) ?? "title";
-    }
-    return this.title;
-  }
+  async onConnect(conn: Party.Connection) {
 
-  getOpts() {
-    // options must match when calling unstable_getYDoc and onConnect
-    const opts: YPartyKitOptions = {
-      callback: { handler: (doc) => this.handleYDocChange(doc) },
-    };
-    return opts;
+    return onConnect(conn, this.party, {
+      // experimental: persists the document to partykit's room storage
+      callback: {
+        handler: (doc) => {
+          console.log("doc", doc)
+        }
+      },
+      persist: { mode: "snapshot" }
+    });
   }
 
   /**
    * Responds to HTTP requests to /parties/chatroom/:roomId endpoint
    */
   async onRequest(request: Party.Request) {
-    const title = await this.loadTitle()
-    if (request.method === "GET") {
-      return json(syncTitle(title))
-    }
-
     // mark room as created by storing its id in object storage
     if (request.method === "POST") {
-      // respond to authentication requests proxied through the app's
-      // rewrite rules. See next.config.js in project root.
-      if (new URL(request.url).pathname.endsWith("/auth")) {
-        // await this.authenticateUser(request);
-        return ok();
-      }
-
       await this.party.storage.put("id", this.party.id);
+      await this.party.storage.put("title", this.party.id);
       return ok();
+    }
+
+    if (request.method === "GET") {
+      console.log("GET", await request.text())
+      return json({
+        title: await this.party.storage.get("title"),
+      });
     }
 
     // respond to cors preflight requests
@@ -57,31 +46,11 @@ export default class EditorServer implements Party.Server {
 
     return notFound();
   }
-  async onConnect(conn: Party.Connection) {
-    await this.updateCount();
-    return onConnect(conn, this.party, this.getOpts());
-  }
+
 
   async onClose(_: Party.Connection) {
-    await this.updateCount();
+    // do what?
+    console.log("document closed")
   }
 
-  handleYDocChange(_: Doc) {
-    //console.log("ydoc changed");
-    // called on every ydoc change
-    // no-op
-  }
-
-  async updateCount() {
-    // Count the number of live connections
-    const count = [...this.party.getConnections()].length;
-    console.log(`we now have ${count} connections in ${this.party.id}`)
-
-    // // Send the count to the 'rooms' party using HTTP POST
-    // await this.party.context.parties.rooms.get(SINGLETON_ROOM_ID).fetch({
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify({ room: this.room.id, count }),
-    // });
-  }
 }
--- party/utils/response.ts
+++ party/utils/response.ts
@@ -1,6 +1,7 @@
 const CORS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
+  "Access-Control-Allow-Headers": "Content-Type",
 };
 
 export const json = <T>(data: T, status = 200) =>
